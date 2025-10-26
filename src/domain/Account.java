e6c7b65a7ec9d8ded644a45223e455fe7939b168
imabhi165 <abhi7808060981@gmail.com>
2025-10-26 15:33:42 +0530

    Added Account for banking app.

--- src/domain/Account.java
+++ src/domain/Account.java
@@ -1,4 +1,17 @@
 package domain;
 
+import java.awt.*;
+
 public class Account {
+    private String accountNumber;
+    private String customerId;
+    private Double balance;
+    private String accountType;
+
+    public Account(String accountNumber, String customerId, Double balance, String accountType) {
+        this.accountNumber = accountNumber;
+        this.customerId = customerId;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
 }
